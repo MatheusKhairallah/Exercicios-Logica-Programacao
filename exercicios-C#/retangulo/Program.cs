4d02a6f22e3365758295a36a1fbe2d6b93e36c3b
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2025-06-07 14:46:40 -0300

    Adicionamento dos exercícios restando em C#

---
+++ exercicios-C#/retangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double baseRetangulo, altura, area, perimetro, diagonal;
+
+            Console.Write("Base do retangulo: ");
+            baseRetangulo = double.Parse(Console.ReadLine()!, CI);
+            Console.Write("Altura do retangulo: ");
+            altura = double.Parse(Console.ReadLine()!, CI);
+
+            area = baseRetangulo * altura;
+            perimetro = 2 * (baseRetangulo + altura);
+            diagonal = Math.Sqrt(Math.Pow(baseRetangulo, 2) + Math.Pow(altura, 2));
+
+            Console.WriteLine("Area = " + area.ToString("F4", CI));
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F4", CI));
+            Console.WriteLine("Diagonal = " + diagonal.ToString("F4", CI));
+
+        }
+    }
+}
---
+++ exercicios-C#/soma_impares/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Soma_impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int X, Y, soma = 0, menor, maior, i;
+
+            Console.WriteLine("Digite dois numeros:");
+            X = int.Parse(Console.ReadLine()!);
+            Y = int.Parse(Console.ReadLine()!);
+
+            if (X < Y)
+            {
+                menor = X;
+                maior = Y;
+            }
+            else
+            {
+                menor = Y;
+                maior = X;
+            }
+
+            for (i = menor+1; i < maior; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+
+            Console.Write("Soma dos impares = " + soma);
+        }
+    }
+}
