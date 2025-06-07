6d52edba784575755ef82ae78edf68e12bccabd0
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2025-06-07 14:44:18 -0300

    Adicionamento dos exercícios feitos em linguagem C#

---
+++ exercicios-C#/crescente/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Crescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int X, Y;
+
+            Console.WriteLine("Digite dois numeros:");
+            X = int.Parse(Console.ReadLine()!);
+            Y = int.Parse(Console.ReadLine()!);
+
+            while (X != Y)
+            {
+                if (X > Y)
+                {
+                    Console.WriteLine("Decrescente!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Crescente!!");
+                }
+
+                Console.WriteLine("Digite outros dois numeros:");
+                X = int.Parse(Console.ReadLine()!);
+                Y = int.Parse(Console.ReadLine()!);
+            }
+        }
+    }
+}
---
+++ exercicios-C#/diagonal_negativos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Diagonal_negativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, i, j, soma;
+
+            Console.Write("Qual a ordem da matriz? ");
+            N = int.Parse(Console.ReadLine()!);
+
+            int[,] mat = new int[N, N];
+
+            for (i = 0; i < N; i++)
+            {
+                for (j = 0; j < N; j++)
+                {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine()!);
+                }
+            }
+
+            Console.WriteLine("Diagonal Principal:");
+            for (i = 0; i < N; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            soma = 0;
+            for (i = 0; i < N; i++)
+            {
+                for (j = 0; j < N; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        soma++;
+                    }
+                }
+            }
+            Console.Write("\nQuantidade de negativos = " + soma);
+        }
+    }
+}
